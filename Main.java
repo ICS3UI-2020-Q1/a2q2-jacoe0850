bc97c14ccedc7d6a28f7f86091f9afe8e00f5ca0
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-15 12:32:20 +0000

    Finished odd/even number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Determines if a given number is odd or even
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Made a constant that odd numbers = 1
+    final int odd = 1;
+    Scanner input = new Scanner(System.in);
+    // Ask for an interger
+    System.out.println("Please enter an integer:");
+    int num = input.nextInt();
+    // calculate the remainder
+int even = num % 2;
+// if statement that will tell you if its odd or not
+     if (even >= odd){
+      System.out.println(num + " is an odd number");
+    } else {
+      System.out.println(num + " is an even number");
+    }
+
+
+
   }
 }
